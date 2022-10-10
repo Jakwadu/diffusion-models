a6019db34def80dc6a69c12a9c0e74f7c6320c2e
Jakwadu <44432334+Jakwadu@users.noreply.github.com>
2022-10-10 13:46:14 +0000

    Update train.py
    Made more object oriented to help keep things modular

--- train.py
+++ train.py
@@ -30,35 +30,72 @@ def spectrogram2audio(data, save=True, mels=True):
         transforms.reverse()
     for transform in transforms:
         data = transform(data)
-    saved = False
-    idx = 0
-    while not saved:
-        f = f'./generated_{artist}_audio_{idx}.mp3'
-        if os.path.exists(f):
-            idx += 1
-            continue
-        torchaudio.save(f, data, sample_rate=44100)
-        saved = True
+    if save:
+        saved = False
+        idx = 0
+        while not saved:
+            f = f'./generated_{artist}_audio_{idx}.mp3'
+            if os.path.exists(f):
+                idx += 1
+                continue
+            torchaudio.save(f, data, sample_rate=44100)
+            saved = True
     return data
 
-def train_diffusion_model(model, training_steps=100000):
-    ...
+class AudioClipGenerator:
+    def __init__(self, 
+                 audio_dir, 
+                 model, 
+                 training_batch_size=16,
+                 diffusion_timesteps=1000,
+                 training_steps=100000):
+        
+        self.configure_training(audio_dir, 
+                                training_batch_size, 
+                                diffusion_timesteps,
+                                training_steps)
+        self.model = model
+
+    def configure_training(self, dir, training_batch_size, diffusion_timesteps, training_steps):
+        training_data = load_audio(dir, mel=USE_MELS)
+        training_data = [data.view(-1, *data.size()) for data in training_data]
+        training_data = torch.concat(training_data)
+
+        self.min_val_ = training_data.min()
+        self.val_range_ = training_data.max() - training_data.min()
+        training_data = (training_data - self.min_val_) / self.val_range_
+
+        self.old_dims = training_data.size()[-2:]
+        self.max_dim = max(self.old_dims)
+        training_data = resize(training_data, (self.max_dim, self.max_dim))
+
+        self.dataset = SpectrogramDataset(training_data)
+
+        self.diffusion = GaussianDiffusion(
+            self.model,
+            image_size=self.max_dim,
+            timesteps=diffusion_timesteps,
+            loss_type='l1'
+        ).cuda()
+
+        self.trainer = Trainer(self.diffusion, 
+                               dataset=self.dataset, 
+                               train_batch_size=training_batch_size,
+                               train_num_steps=training_steps)
+
+    def train(self):
+        self.trainer.train()
+
+    def sample(self, batch_size=5, save_audio=True):
+        samples = self.diffusion.sample(batch_size=batch_size)
+        samples = resize(samples, self.old_dims)
+        samples = samples * self.val_range_ + self.min_val_
+        generated_audio = [spectrogram2audio(samples[idx], save=save_audio) for idx in range(len(samples))]
+        return generated_audio
+
 
 if __name__ == '__main__':
     music_dir = f'/home/user/Projects/spotify-machine-learning/all_artists/{artist}'
-    training_data = load_audio(music_dir, mel=USE_MELS)
-    training_data = [data.view(-1, *data.size()) for data in training_data]
-    training_data = torch.concat(training_data)
-
-    min_val_ = training_data.min()
-    val_range_ = training_data.max() - training_data.min()
-    training_data = (training_data - min_val_) / val_range_
-
-    old_dims = training_data.size()[-2:]
-    max_dim = max(old_dims)
-    training_data = resize(training_data, (max_dim, max_dim))
-
-    training_dataset = SpectrogramDataset(training_data)
 
     model = Unet(
         dim=8,
@@ -66,17 +103,6 @@ if __name__ == '__main__':
         dim_mults=(1,2)
     ).cuda()
 
-    diffusion = GaussianDiffusion(
-        model,
-        image_size=max_dim,
-        timesteps=1000,
-        loss_type='l1'
-    ).cuda()
+    audio_generator = AudioClipGenerator(music_dir, model)
 
-    trainer = Trainer(diffusion, dataset=training_dataset, train_batch_size=4)
-    trainer.train()
-
-    samples = diffusion.sample(batch_size=5)
-    samples = resize(samples, old_dims)
-    samples = samples * val_range_ + min_val_
-    generated_audio = [spectrogram2audio(samples[idx] for idx in range(len(samples)))]
+    samples = audio_generator.sample(batch_size=5)
